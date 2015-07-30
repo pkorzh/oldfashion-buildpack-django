308f0099001b8aa190a8dd32b60d8e5af707190e
Platon Korzh <platon.korzh@gmail.com>
2015-07-30 14:19:33 +0300

    added buildpack class

--- __init__.py
+++ __init__.py
@@ -1,2 +1,8 @@
-def detect(files):
-	return 'manage.py' in files
+import pkg_resources
+
+class Buildpack():
+	def detect(self, files):
+		return 'manage.py' in files
+
+	def get_build_context_path(self, files):
+		return pkg_resources.resource_filename(__name__, "build-context")
